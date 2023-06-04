c22ee065213bdbf20c1e5ebaf956073f3a770395
Raihan Fitrah <raihanfitrah15@gmail.com>
2023-06-05 00:18:54 +0700

    ToW 1.1
    So many progress has been made that i dont even remember the details, but
    
    added
    - Player movement
    - Player walk animation
    - Background
    - Tilemap

---
+++ Assets/Mbee.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mbee : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Long live Leithania");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    Vector2 moveInput;
+
+    [SerializeField]
+    private bool _isMoving = false;
+
+    public bool IsMoving { get 
+        { 
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        }
+
+    }
+
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight { get { return _isFacingRight; } private set
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            _isFacingRight = value;
+        }
+    }
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    }
+
+    void OnAttack()
+    {
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if(moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+}
